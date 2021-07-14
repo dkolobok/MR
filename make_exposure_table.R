5bd7a228ee3978365dbfc5db748f4599c41bca5d
dkolobok <dmitry.s.kolobkov@gmail.com>
2021-07-14 15:15:32 +0300

    upd

--- make_exposure_table.R
+++ make_exposure_table.R
@@ -4,8 +4,9 @@ library(dplyr)
 source('config.R')
 
 # read available outcomes table from cache or from the web
-ao_fn <- paste(data_folder, 'mr_available_outcomes.csv', sep='/')
+ao_fn <- paste(data_dir, 'mr_available_outcomes.csv', sep='/')
 if (file.exists(ao_fn)) ao <- read.csv(ao_fn) else {
+  library(TwoSampleMR)
   log_info('Caching available outcomes...')
   ao <- available_outcomes()
   write.csv(ao, ao_fn, row.names = FALSE)
@@ -14,7 +15,7 @@ if (file.exists(ao_fn)) ao <- read.csv(ao_fn) else {
 # the original string database table can be processed by 
 # sed 's/;.*//; s/9606.//' 9606.protein.info.v11.0.txt > stringdb.txt
 # (delete everything after ; and delete '9606.' prefix)
-string_db <- read.csv(paste(data_folder, 'stringdb.txt', sep='/'), sep='\t')
+string_db <- read.csv(paste(data_dir, 'stringdb.txt', sep='/'), sep='\t')
 string_db <- string_db %>% dplyr::rename(protein_name = annotation)
 
 exposure_df <- ao %>% dplyr::filter(stringr::str_detect(id, 'prot-')) %>% 
@@ -24,17 +25,17 @@ exposure_df <- ao %>% dplyr::filter(stringr::str_detect(id, 'prot-')) %>%
 exposure_df <- exposure_df %>% inner_join(string_db, by='protein_name')
 
 exposure_df %>% dplyr::select(protein_external_id) %>% distinct() %>% 
-  write.csv(paste(data_folder, 'ensp.csv', sep='/'), row.names = FALSE)
+  write.csv(paste(data_dir, 'ensp.csv', sep='/'), row.names = FALSE)
 
 # run mygene_coordinates.py
-protein_gene_coordinates <- read.csv(paste(data_folder, 'protein_gene_coordinates.csv', sep='/')) %>% 
+protein_gene_coordinates <- read.csv(paste(data_dir, 'protein_gene_coordinates.csv', sep='/')) %>% 
   dplyr::rename(protein_external_id = query)
 
 exposure_df <- exposure_df %>% inner_join(protein_gene_coordinates, by='protein_external_id') %>% 
   dplyr::select(id, preferred_name, chr, start, end)
 
 exposure_df <- exposure_df %>% rename(id.exposure = id)
-write.csv(exposure_df, paste(data_folder, 'selected_exposures.csv', sep='/'), row.names = FALSE)
+write.csv(exposure_df, paste(data_dir, 'selected_exposures.csv', sep='/'), row.names = FALSE)
 
 # 25: 161
 # indf <- exposure_df %>% mutate(id.outcome = 'ieu-b-69') %>% 
